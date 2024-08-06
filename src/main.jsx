110eea277907f4ade6474a119e88ca0a00f20209
Wilfredo <wilvargas7@gmail.com>
2024-08-06 15:48:39 -0500

    remove HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GifExpertApp } from './GifExpertApp';
 import './styles.css';
-import { HashRouter } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
       <GifExpertApp/>
-    </HashRouter>  
   </React.StrictMode>,
 );
