b85ee95bb0ca5e1fb1fff9f9222e24e892346cf5
Wilfredo <wilvargas7@gmail.com>
2024-08-06 13:44:46 -0500

    Add '<HashRouter>'

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GifExpertApp } from './GifExpertApp';
 import './styles.css';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GifExpertApp/>
+    <HashRouter>
+      <GifExpertApp/>
+    </HashRouter>  
   </React.StrictMode>,
 );
