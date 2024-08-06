8aef7a9bc2cf0057d73105a858d80c1b175793ba
Wilfredo <wilvargas7@gmail.com>
2024-08-06 12:57:42 -0500

    slash de cierre al nombre de mi repo en base:

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-gif-expert',
+  base: '/react-gif-expert/',
   build: {
     outDir: 'docs' // Cambia la carpeta de salida a 'docs'
   }
