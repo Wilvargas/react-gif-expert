cec3abbe5672a34aed51c8f768629aed78fcb753
Wilfredo <wilvargas7@gmail.com>
2024-08-07 17:47:55 -0500

    UI Testing a los componentes getGifs.js, GifItem.jsx, AddCategory.jsx

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,12 +1,32 @@
-import { render } from "@testing-library/react"
+import { getByText, render, screen } from "@testing-library/react"
 import { GifItem } from "../../src/components/GifItem"
 
 describe('Pruebas en <GifItem />', () => {
 
     const title = 'Saitama';
     const url = 'https://one-punch.com/saitama.jpg'
+
     test('debe hacer match con el snapshot', () => {
 
-        render( <GifItem title="{ title }" url="{ url }"/> )
+        const { container } = render( <GifItem title="{ title }" url="{ url }"/> )
+        expect(container).toMatchSnapshot();
+    })
+
+    test('debe mostrar la imagen de la url y el ALT indicado', () => {
+        
+        render( <GifItem title={ title } url={ url }/> );
+        // screen.debug();
+        // expect( screen.getByRole('img').src ).toBe( url );
+        // expect( screen.getByRole('img').alt ).toBe( title );
+        const { src, alt } = screen.getByRole('img');
+        expect(src).toBe( url );
+        expect(alt).toBe( title );
+    })
+
+    test('debe mostrar el título en el componente', () => {
+
+        render( <GifItem title={ title } url={ url }/> );
+        expect( screen.getByText( title ) ).toBeTruthy();
+        // screen.debug();
     })
 })
